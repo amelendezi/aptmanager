b3f2aa92a0e03800d82f79f8e5883f3526dbea42
Alberto Melendez <alberto.melendez@afas.nl>
2018-07-21 15:59:14 +0200

    Fixed module error.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PersonComponent } from './person/person.component';
 import { PeopleComponent } from './people/people.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonComponent,
     PeopleComponent
   ],
   imports: [
