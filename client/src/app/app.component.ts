6d22c813438c8ac2afd4bc47f88a7711f1f46c2f
Alberto Melendez <alberto.melendez@afas.nl>
2018-07-21 15:31:39 +0200

    Implementation of the appComponent class
    Basic structure of navigation using the angular directives being setup.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Landing Page';
+
+  overviewCode = 'overview';
+  overviewOption = false;
+
+  apartmentsCode = 'apartments';
+  apartmentsOption = false;
+
+  buildingCode = 'building';
+  buildingOption = false;
+
+  suppliersCode = 'suppliers';
+  suppliersOption = false;
+
+  peopleCode = 'people';
+  peopleOption = true;
+
+  menuItemStyle = 'main-menu-item';
+  menuItemSelectedStyle = 'main-menu-item-selected';
+
+  /**
+   * Sets the CSS style to a given main menu item depending on if it
+   * is selected or not.
+   */
+  getMenuItemStyle(option: string): string {
+    switch (option) {
+      case this.overviewCode:
+        return this.overviewOption ? this.menuItemSelectedStyle : this.menuItemStyle;
+      case this.apartmentsCode:
+        return this.apartmentsOption ? this.menuItemSelectedStyle : this.menuItemStyle;
+      case this.buildingCode:
+        return this.buildingOption ? this.menuItemSelectedStyle : this.menuItemStyle;
+      case this.suppliersCode:
+        return this.suppliersOption ? this.menuItemSelectedStyle : this.menuItemStyle;
+      case this.peopleCode:
+        return this.peopleOption ? this.menuItemSelectedStyle : this.menuItemStyle;
+    }
+    console.log('Loading styles for main menu');
+    return this.menuItemStyle;
+  }
+
+  toggleOption(option: string): void {
+    console.log('The option is: ' + option);
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonComponent } from './person/person.component';
+import { PeopleComponent } from './people/people.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonComponent,
+    PeopleComponent
   ],
   imports: [
     BrowserModule
