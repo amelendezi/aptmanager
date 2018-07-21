9e2724dda871a64fe528eaedf870d0aed4031732
Alberto Melendez <alberto.melendez@afas.nl>
2018-07-21 16:23:59 +0200

    Completed skeleton navigation

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PeopleComponent } from './people/people.component';
+import { OverviewComponent } from './overview/overview.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
+import { BuildingComponent } from './building/building.component';
+import { SuppliersComponent } from './suppliers/suppliers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PeopleComponent
+    PeopleComponent,
+    OverviewComponent,
+    ApartmentsComponent,
+    BuildingComponent,
+    SuppliersComponent
   ],
   imports: [
     BrowserModule
