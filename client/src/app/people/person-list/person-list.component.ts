886aec6f5cd654b99652fea15e7bdfbb23fd8de7
Alberto Melendez <alberto.melendez@afas.nl>
2018-07-26 09:48:24 +0200

    Adjusted list item css styles

--- client/src/app/people/person-list/person-list.component.ts
+++ client/src/app/people/person-list/person-list.component.ts
@@ -9,20 +9,39 @@ export class PersonListComponent implements OnInit {
 
   persons: any[] = [
     {
-      fullName: 'John Conor',
-      email: 'j.conor@terminator.com',
+      fullName: 'John Connor',
+      email: 'j.connor@terminator.com',
+      phone: '+57 300 2184225',
       profileUrl: 'sample.jpg',
     },
     {
       fullName: 'Rocky Balboa',
       email: 'r.balboa@rocky.com',
+      phone: '+57 301 2564879',
       profileUrl: 'sample.jpg',
-    }
+    },
+    {
+      fullName: 'Jason Clarke',
+      email: 'j.clarke@holis.com',
+      phone: '+52 06 25586978',
+      profileUrl: 'sample.jpg',
+    },
+    {
+      fullName: 'Andrea Negro',
+      email: 'anegro@nnt.er',
+      phone: '+37 305 2589878',
+      profileUrl: 'sample.jpg',
+    },
+    {
+      fullName: 'Silvia Furlong',
+      email: 'silvia@urton.com',
+      phone: '+37 521 2564879',
+      profileUrl: 'sample.jpg',
+    },
   ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
