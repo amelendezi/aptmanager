f40a258e937cdbecfcdbd4623a442645d326f729
Alberto Melendez <alberto.melendez@afas.nl>
2018-07-28 14:56:34 +0200

    Binded the nested person item component to person instead of fields

--- client/src/app/people/person-list/person-list-item/person-list-item.component.ts
+++ client/src/app/people/person-list/person-list-item/person-list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnChanges, Input } from '@angular/core';
+import { Person } from '../../model/person';
 
 @Component({
   selector: 'app-person-list-item',
@@ -7,13 +8,17 @@ import { Component, OnInit, OnChanges, Input } from '@angular/core';
 })
 export class PersonListItemComponent implements OnInit, OnChanges {
 
-  @Input() title: string;
-  @Input() subTitle: string;
-  @Input() profilePictureUrl: string;
+  title: string;
+  subTitle: string;
+  profilePictureUrl: string;
+  @Input() person: Person;
 
   constructor() { }
 
   ngOnChanges(): void {
+    this.profilePictureUrl = this.person.profileUrl;
+    this.title = this.person.getFullName();
+    this.subTitle = this.person.email + ' | ' + this.person.phone;
   }
 
   ngOnInit(): void {
